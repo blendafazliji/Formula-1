8a622305c32c2a66097cba8091e6f184dccdc286
blendafazliji <146988941+blendafazliji@users.noreply.github.com>
2026-05-23 21:02:42 +0200

    Add various MongoDB aggregate queries for races

--- nosql_queries/mongodb_queries.js
+++ nosql_queries/mongodb_queries.js
@@ -1,3 +1,4 @@
+# Top drivers by wins
 db.races.aggregate([
   { $unwind: "$results" },
 
@@ -30,3 +31,142 @@ db.races.aggregate([
     }
   }
 ])
+
+# Top constructors   write a sql and a mongodb query
+db.races.aggregate([
+  { $unwind: "$results" },
+
+  {
+    $match: {
+      "results.position": 1
+    }
+  },
+
+  {
+    $group: {
+      _id: "$results.constructorName",
+      wins: { $sum: 1 }
+    }
+  },
+
+  { $sort: { wins: -1 } },
+
+  { $limit: 10 },
+
+  {
+    $project: {
+      _id: 0,
+      constructor: "$_id",
+      wins: 1
+    }
+  }
+])
+
+# Most races participated
+db.races.aggregate([
+  { $unwind: "$results" },
+
+  {
+    $group: {
+      _id: {
+        race: "$raceid",
+        driver: "$results.driverid"
+      }
+    }
+  },
+
+  {
+    $group: {
+      _id: "$_id.driver",
+      totalRaces: { $sum: 1 }
+    }
+  },
+
+  { $sort: { totalRaces: -1 } }
+])
+
+# Circuit with most races
+db.races.aggregate([
+  {
+    $group: {
+      _id: "$circuit.circuitId",
+      circuitName: { $first: "$circuit.name" },
+      totalRaces: { $sum: 1 }
+    }
+  },
+
+  { $sort: { totalRaces: -1 } },
+
+  { $limit: 1 },
+
+  {
+    $project: {
+      _id: 0,
+      circuitId: "$_id",
+      circuitName: 1,
+      totalRaces: 1
+    }
+  }
+])
+
+# Average finishing position per driver
+db.races.aggregate([
+  { $unwind: "$results" },
+
+  {
+    $match: {
+      "results.position": { $ne: null }
+    }
+  },
+
+  {
+    $group: {
+      _id: {
+        driverId: "$results.driverid",
+        driverName: "$results.driverName"
+      },
+      avgPosition: { $avg: "$results.position" }
+    }
+  },
+
+  { $sort: { avgPosition: 1 } },
+
+  { $limit: 10 },
+
+  {
+    $project: {
+      _id: 0,
+      driverId: "$_id.driverId",
+      driverName: "$_id.driverName",
+      avgPosition: 1
+    }
+  }
+])
+
+# Total points per constructor 
+db.races.aggregate([
+  { $unwind: "$results" },
+
+  {
+    $group: {
+      _id: {
+        constructorId: "$results.constructorid",
+        constructorName: "$results.constructorName"
+      },
+      totalPoints: { $sum: "$results.points" }
+    }
+  },
+
+  { $sort: { totalPoints: -1 } },
+
+  {
+    $project: {
+      _id: 0,
+      constructorId: "$_id.constructorId",
+      constructorName: "$_id.constructorName",
+      totalPoints: 1
+    }
+  }
+])
+
+
